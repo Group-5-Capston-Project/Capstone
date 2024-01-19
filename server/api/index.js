f9aba07d4e17895fc50b5d4bf50e683027947745
Rafael Sawiris <rafaelsawiris@Rafaels-MacBook-Pro.local>
2024-01-19 07:44:15 -0800

    still working on user functionality

--- server/api/index.js
+++ server/api/index.js
@@ -8,7 +8,6 @@ app.use('/products', require('./products'));
 app.use('/', require('./auth'));
 app.use('/orders', require('./orders'));
 app.use('/lineItems', require('./lineItems'));
-
 app.use('/wishlist', require('./wishlist'));
 
 
