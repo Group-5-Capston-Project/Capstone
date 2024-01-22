eb4118c5a5cb5e2e89c55d05b64461552ab0c407
Ruba <ruba@Rubas-MacBook-Pro.local>
2024-01-22 08:32:39 -0800

    styling

--- server/api/wishlist.js
+++ server/api/wishlist.js
@@ -2,22 +2,22 @@ const {
     fetchWishListItems,
     addToWishList,
     removeFromWishList,
-} = require('../db/wishlist');
-
-const express = require('express');
-const app = express.Router();
-const { isLoggedIn, isAdmin } = require('./middleware');
-
-app.get('/:userId', isLoggedIn, async (req, res, next) => {
+  } = require('../db/wishlist');
+  
+  const express = require('express');
+  const app = express.Router();
+  const { isLoggedIn, isAdmin } = require('./middleware');
+  
+  app.get('/:userId', isLoggedIn, async (req, res, next) => {
     try {
         res.send(await fetchWishListItems(req.params.userId));
     }
     catch(ex){
         next(ex);
     }
-});
-
-app.post('/add', isLoggedIn, async (req, res, next) => {
+  });
+  
+  app.post('/add', isLoggedIn, async (req, res, next) => {
     try {
         res.send(await addToWishList(req.body.userId, req.body.productId));
         res.status(201);
@@ -25,9 +25,9 @@ app.post('/add', isLoggedIn, async (req, res, next) => {
     catch(ex){
         next(ex);
     }
-});
-
-app.delete('/remove/:userId/:productId', isLoggedIn, async (req, res, next) => {
+  });
+  
+  app.delete('/remove/:userId/:productId', isLoggedIn, async (req, res, next) => {
     try {
         await removeFromWishList(req.params.userId, req.params.productId);
         res.sendStatus(204);
@@ -35,6 +35,6 @@ app.delete('/remove/:userId/:productId', isLoggedIn, async (req, res, next) => {
     catch(ex){
         next(ex);
     }
-});
-
-module.exports = app;
+  });
+  
+  module.exports = app;
