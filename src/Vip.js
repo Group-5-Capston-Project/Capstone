7bbcbc5d23471bbfd4fe5c58e119878012bd7870
Rafael Sawiris <rafaelsawiris@Rafaels-MacBook-Pro.local>
2024-01-25 09:57:51 -0800

    vip products page

--- src/Vip.js
+++ src/Vip.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const handleSubmit = () => {
-    console.log('hello')
+    console.log('hello!')
 }
 
 const VipUsers = ({auth, vip_products}) => {
