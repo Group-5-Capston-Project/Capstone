d122a47b7f54cd89b5fbab42d0ed535c029c08e4
fk4321 <139650641+fk4321@users.noreply.github.com>
2024-01-30 11:59:22 -0500

    gave admin access to view vip products

--- src/Vip.js
+++ src/Vip.js
@@ -7,7 +7,7 @@ const VipUsers = ({products, cartItems, createLineItem, updateLineItem, auth, up
     const navigate = useNavigate();
     return (
         <div>
-            {auth.is_vip ? 
+            {auth.is_vip || auth.is_admin ? 
                 <div className='page-users '>
                     <h2 className='pagetitle'> Thank you {auth.username}! Enjoy only VIP Products Below!</h2>
 
