6eeb37d25007015e7345a5d42679303be38c1c65
Ruba <ruba@Rubas-MacBook-Pro.local>
2024-01-31 08:48:54 -0800

    styled shipping page

--- src/OrderCon.js
+++ src/OrderCon.js
@@ -1,47 +1,51 @@
 import React from 'react';
 import { Link, Route, Routes, useNavigate } from 'react-router-dom';
 
-const OrderCon = ({  orders , address , setAddress }) => {
+const OrderCon = ({ orders, address, setAddress }) => {
 
-    const navigate = useNavigate();
-    return (
-      <div >
-        <h2>Thank you for your order! </h2>
-        <ul> Confirmation #
-        { orders.map( order => {
-            return (
-              <li key={ order.id }> 
-                { order.id }
-              </li>
-            );
-          }) }
-       </ul>
-       <h2> Address </h2>
-       <ul>  
-        { address.map( add => {
-            return (
-              <li key={ add.id }>
-                { add.name }
-                <br></br> 
-                { add.last_name }
-                <br></br> 
-                { add._address }
-                <br></br> 
-                { add.phone }
-              </li>
-            );
-          }) }
-       </ul>
+  const navigate = useNavigate();
+  return (
+    <div className='page-users'>
+      <h2 className='pagetitletwo'>Thank you for your order! </h2>
+      <ul className='shippingcontainer'> 
+      <h2 className='formtitle'>Confirmation #</h2>
+        {orders.map(order => {
+          return (
+            <li key={order.id} >
+              {order.id}
+            </li>
+          );
+        })}
+      </ul>
+      <h2 className='shippingheader'> Address </h2>
+      <ul className='shippinglist'>
+        {address.map(add => {
+          return (
+            <li key={add.id} className='address'>
+              {add.name}
+              <br></br>
+              {add.last_name}
+              <br></br>
+              {add._address}
+              <br></br>
+              {add.phone}
+            </li>
+          );
+        })}
+      </ul>
 
-       
-
-        <Link to ="/products"> Back to home </Link>
-       
-       
-        
-     
+      <div className='center'>
+        <Link to="/products" className='backbuttonthree'> Back to all products </Link>
       </div>
-    );
-  };
-  
-  export default OrderCon;
+
+
+
+
+
+
+
+    </div>
+  );
+};
+
+export default OrderCon;
